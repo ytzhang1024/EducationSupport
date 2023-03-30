026259c951792011a859de3756ca3f7c1547a8e1
ytzhang1024 <tim.y.zhang@outlook.com>
2023-03-30 17:43:00 +0100

    v0

---
+++ app/src/main/java/com/android/example/educationsupport/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.android.example.educationsupport.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.example.educationsupport.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
